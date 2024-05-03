77db75487eb80178d21105722fc5f33f6732fd68
Tiago Silveira De Bittencourt <tiago.40123@alunosatc.edu.br>
2024-05-03 19:40:45 -0300

    feat(scripts): Adds a new start script in the package.json for production builds

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { entities } from './entities.list';
 import { migrations } from './migrations.list';
+import { env } from 'process';
 
 export const AppDataSource = new DataSource({ 
     type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'stocksense',
+    host: env.MYSQLHOST || '127.0.0.1',
+    port: Number(env.MYSQLPORT) || 3306,
+    username: env.MYSQLUSER || 'root',
+    password: env.MYSQLPASSWORD || '',
+    database: env.MYSQL_DATABASE || 'stocksense',
     synchronize: false,
     logging: false,
     entities: entities,
