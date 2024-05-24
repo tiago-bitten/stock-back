7baa1ab9a26410e108f988620a417e673d8f0015
gus <dev@ninsaude.com>
2024-05-24 20:40:33 -0300

    feat: :sparkles: Implemented user role authentication on authmiddleware, changed the ENUM of nivel to USER, ADMIN

--- src/app/repositories/UsuarioRepository.ts
+++ src/app/repositories/UsuarioRepository.ts
@@ -2,18 +2,29 @@ import Usuario from '../models/Usuario';
 import { AppDataSource } from '../../database/data-source';
 import IUsuario from '../interfaces/IUsuario';
 import { DeepPartial } from 'typeorm';
+import ICargo from '../interfaces/ICargo';
 
 const userRepository = AppDataSource.getRepository(Usuario);
 
 class UsuarioRepository {
     public getUsers = (): Promise<IUsuario[]> => {
-        return userRepository.find();
+        return userRepository.find({ relations: ['empresa', 'cargo'] });
     };
     
     public getUser = ({id, email}: {id?: number, email?: string}): Promise<IUsuario | null> => {
         const whereClause = id ? { id } : email ? { email } : null;
         return whereClause ? userRepository.findOne({ where: whereClause, relations: ['empresa', 'cargo'] }) : Promise.resolve(null);
     };
+
+    public getUserRole = async (id: number): Promise<'ADMIN' | 'USER' | null> => {
+        const user: IUsuario | null = await userRepository.findOne({ where: { id }, relations: ['cargo'] });
+    
+        if (!user || !user.cargo) return null;
+    
+        const userRole = (user.cargo as ICargo).nivel;
+    
+        return userRole;
+    }
     
     public createNewUser = (user: IUsuario) => {
         const newUser = userRepository.create(user as DeepPartial<Usuario>);
--- src/app/routes/UsuarioRouter.ts
+++ src/app/routes/UsuarioRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import UsuarioController from "../controllers/UsuarioController";
 import authMiddleware from "../middlewares/authMiddleware";
-import permissionsMiddleware from "../middlewares/permissionsMiddleware";
 
 const router = Router();
 
-router.get('/usuario', authMiddleware(), permissionsMiddleware(), UsuarioController.getUsers);
+router.get('/usuario', authMiddleware(), UsuarioController.getUsers);
 router.post('/usuario', UsuarioController.storeUser);
 
 export default router;
