5651952cf2df06504591a953f5b6c557c880d2d9
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2024-11-23 14:52:58 -0300

    fix

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,24 +6,24 @@ import { migrations } from './migrations.list';
 
 dotenv.config();
 
-if (
-  !process.env.DB_HOST ||
-  !process.env.DB_PORT ||
-  !process.env.DB_USER ||
-  !process.env.DB_PASS ||
-  !process.env.DB_NAME
-) {
+const DB_HOST = process.env.MYSQLHOST || process.env.DB_HOST;
+const DB_PORT = Number(process.env.MYSQLPORT) || Number(process.env.DB_PORT);
+const DB_USER = process.env.MYSQLUSER || process.env.DB_USER;
+const DB_PASS = process.env.MYSQLPASSWORD || process.env.DB_PASS;
+const DB_NAME = process.env.MYSQLDATABASE || process.env.DB_NAME;
+
+if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASS || !DB_NAME) {
   throw new Error('Algumas variáveis de ambiente do banco de dados estão ausentes.');
 }
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: false, 
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  synchronize: false,
   logging: true,
   entities: entities,
   migrations: migrations,
